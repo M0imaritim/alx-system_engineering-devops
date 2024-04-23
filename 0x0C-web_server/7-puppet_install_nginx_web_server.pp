79c5b7ef347aee81c1fae17af327328c24054447
m0imaritim <moimaritim@gmail.com>
2024-04-23 13:03:51 -0700

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+# nginx configuration
+package { 'nginx':
+  ensure => installed,
+}
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+      content => "
+      server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm;
+
+        server_name _;
+
+        location / {
+          return 200 'Hello World!';
+        }
+
+        location = /redirect_me {
+          return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+      }
+      ",
+      require => Package['nginx'],
+}
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}

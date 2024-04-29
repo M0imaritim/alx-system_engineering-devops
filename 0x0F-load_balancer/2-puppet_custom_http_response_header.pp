591ed0cf78c5dfdd236c9a23dd2bad463e580882
m0imaritim <moimaritim@gmail.com>
2024-04-29 15:04:19 -0700

    puppet automation

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,25 @@
+# Configures a brand new ubuntu machine
+
+exec{'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+package { 'nginx':
+  ensure => installed,
+}
+
+$server_hostname = $facts['networking']['hostname']
+$custom_header_value = "X-Served-By: ${server_hostname}"
+
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => present,
+  content => "add_header ${custom_header_value};\n",
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/conf.d/custom_header.conf'],
+}

fff1c6a755eeed7f3af8afacb0603f55b9d3120d
moimaritim <moimaritim@gmail.com>
2025-02-26 19:19:29 +0300

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 #fixes 500 error code on apache web server
 
-file_line { 'fix_php_typo':
-  path    => '/var/www/html/wp-settings.php',  # Target file
-  match   => 'phpp',
-  line    => 'php',
-  replace => true,
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
